2734264540b1528b7968de8fcf8e2918b3d7c947
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2022-09-10 17:47:49 +0200

    comple tests fluxes and grid1

--- test/test_hrutils.f90
+++ test/test_hrutils.f90
@@ -2,7 +2,7 @@ module test_hrutils
 !----------------------------------------------------------------------------------------------
 !! Test for module 'hrutils' using test-drive.
 !----------------------------------------------------------------------------------------------
-        use hrutils, only : godunov, lax_friedrichs 
+        use hrutils, only : godunov, lax_friedrichs, grid1, tgrid1
         use iso_fortran_env, only : real64, error_unit
         use testdrive, only : new_unittest, unittest_type, error_type, check
         implicit none
@@ -21,8 +21,8 @@ module test_hrutils
             type(unittest_type), allocatable, intent(out) :: testsuite(:)
     
             testsuite = [ &
-            new_unittest("fluxes", test_fluxes) &
-            ! new_unittest("mstvd", test_mstvd) &
+            new_unittest("fluxes", test_fluxes), &
+            new_unittest("grid1", test_grid1) &
             ]
     
         end subroutine
@@ -42,7 +42,7 @@ module test_hrutils
             ! Check h(a,a) = h(a)
             write(error_unit, *) "Check h(a,a) = h(a)"  
             vp = vm
-            href = vm*x*t 
+            href = f(vp, x, t)
             h = godunov(f, vm, vp, x, t)
             call check(error, h, href, rel=.true., thr=rtol)
             if (allocated(error)) return
@@ -50,35 +50,70 @@ module test_hrutils
             call check(error, h, href, rel=.true., thr=rtol)
             if (allocated(error)) return
 
-            ! Check wind blows from left to right, h(a,b) =~ h(a)
-            write(error_unit, *) "h(a,b) =~ h(a)"
+            ! Check Check ->
+            write(error_unit, *) "Check ->"
             vp = -2*vm
-            href = vm*x*t 
+            href = f(vm, x, t) 
             h = godunov(f, vm, vp, x, t)
             call check(error, h, href, rel=.true., thr=rtol)
             if (allocated(error)) return
             h = lax_friedrichs(f, vm, vp, x, t, x*t)
-            write(error_unit, *) href, h
             call check(error, h, href, rel=.true., thr=rtol)
             if (allocated(error)) return
-            stop
 
-            ! Check wind blows from right to left, h(a,b) =~ h(b)
+            ! Check Check <-
+            write(error_unit, *) "Check <-"
             vm = - vm
             vp = - vp
-            write(error_unit, *) "h(a,b) =~ h(b)"  
-            href = vp*x*t 
+            href = f(vm, x, t)  
             h = godunov(f, vm, vp, x, t)
-            write(error_unit, *) href, h
             call check(error, h, href, rel=.true., thr=rtol)
             if (allocated(error)) return
-            !h = lax_friedrichs(f, vm, vp, x, t, x*t)
-            write(error_unit, *) href, h
+            h = lax_friedrichs(f, vm, vp, x, t, x*t)
             call check(error, h, href, rel=.true., thr=rtol)
             if (allocated(error)) return
     
         end subroutine test_fluxes
        
+
+        subroutine test_grid1(error)
+            type(error_type), allocatable, intent(out) :: error
+            integer :: nc
+            real(rk) :: xmin, xmax
+            type(tgrid1) :: gx
+    
+            ! General settings
+            xmin = 0._rk
+            xmax = 8._rk
+            nc = 4
+
+            ! Make grid 
+            gx = grid1(xmin, xmax, nc)
+
+            ! print *, "nc", gx%nc
+            ! print *, "edges", gx%edges
+            ! print *, "d", gx%d
+            ! print *, "c", gx%c
+            ! print *, "l", gx%l
+            ! print *, "r", gx%r
+
+            ! Checks
+            call check(error, gx%nc, nc)
+            if (allocated(error)) return
+            call check(error, gx%edges(0), xmin)
+            if (allocated(error)) return
+            call check(error, gx%edges(nc), xmax)
+            if (allocated(error)) return
+            call check(error, gx%l, gx%edges(0:nc-1))
+            if (allocated(error)) return
+            call check(error, gx%r, gx%edges(1:nc))
+            if (allocated(error)) return
+            call check(error, gx%d, gx%r-gx%l)
+            if (allocated(error)) return
+            call check(error, gx%c, (gx%l+gx%r)/2)
+    
+        end subroutine test_grid1
+
         !> Simple flux function to test numerical fluxes
         pure function f(u, x, t)
             real(rk) :: f
