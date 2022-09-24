a806df484101e14f07905d1e5b9b52c0f8af9c55
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2022-09-24 20:03:18 +0200

    fix typo doc

--- src/fluxes.f90
+++ src/fluxes.f90
@@ -34,7 +34,7 @@ contains
       real(rk), intent(in) :: vm
         !! left (minus) reconstruction \( v_{i+1/2}^- \)
       real(rk), intent(in) :: vp
-        !! right (plus) reconstruction \( v_{i+1/2}^+ = v_{(i+1)+1/2}^- \)
+        !! right (plus) reconstruction \( v_{i+1/2}^+ = v_{(i+1)-1/2}^- \)
       real(rk), intent(in) :: x(:)
         !! x at flux interface, \( x_{i+1/2} \)
       real(rk), intent(in) :: t
@@ -58,7 +58,7 @@ contains
       real(rk), intent(in) :: vm
         !! left (minus) reconstruction \( v_{i+1/2}^- \)
       real(rk), intent(in) :: vp
-        !! right (plus) reconstruction \( v_{i+1/2}^+ = v_{(i+1)+1/2}^- \)
+        !! right (plus) reconstruction \( v_{i+1/2}^+ = v_{(i+1)-1/2}^- \)
       real(rk), intent(in) :: x(:)
         !! x at flux interface, \( x_{i+1/2} \)
       real(rk), intent(in) :: t
