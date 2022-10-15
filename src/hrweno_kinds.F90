c83c26efd39fd5ac5791d76ff7055cc4e71b5429
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2022-10-15 12:03:48 +0200

    add intrinsic

--- src/hrweno_kinds.F90
+++ src/hrweno_kinds.F90
@@ -1,6 +1,6 @@
 module hrweno_kinds
 !! Aux module to define real precision.
-   use iso_fortran_env, only: real32, real64, real128
+   use, intrinsic :: iso_fortran_env, only: real32, real64, real128
    implicit none
    private
 
